41340789ce3af2bfcd38aa2c29435ffb91210bb1
Shawn Rhodes <shawn@massivelines.com>
2018-01-12 20:54:53 -0600

    Header text sticks to end of header tag on scroll plus cutout of background

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ function getOffset(el) {
 	};
 }
 
+
 var headerTextOffset = getOffset(headerText);
 var headerOffset = getOffset(headerTag);
 
@@ -22,41 +23,69 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
 
-ctx.moveTo(0, canvas.height);
-ctx.lineTo(0, canvas.height*.33);
-ctx.lineTo(canvas.width, canvas.height*.66);
-ctx.lineTo(canvas.width, canvas.height);
-ctx.fill();
+function CanvasBackground() {
+  this.draw = function () {
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height);
+    ctx.lineTo(0, canvas.height*.33);
+    ctx.lineTo(canvas.width, canvas.height*.66);
+    ctx.lineTo(canvas.width, canvas.height);
+    ctx.fillStyle = "black"
+    ctx.fill();
+  }
+  this.draw();
+}
 
+
+
+
+function CanvasText() {
+  var myNameText = document.getElementById("myName");
+  var myNameOffset = getOffset(myName);
+  var myNameFontSize = window.getComputedStyle(myNameText).fontSize;
+  var myNameFontFamily = window.getComputedStyle(myNameText).fontFamily;
+
+  var taglineText = document.getElementById("tagline");
+  var taglineOffset = getOffset(tagline);
+  var taglineFontSize = window.getComputedStyle(taglineText).fontSize;
+  var taglineFontFamily = window.getComputedStyle(taglineText).fontFamily;
+
+  this.draw = function () {
+    ctx.beginPath();
+    ctx.font = myNameFontSize + " " + myNameFontFamily;
+    ctx.fillStyle = "White";
+    ctx.textBaseline = "bottom";
+    ctx.fillText(myNameText.innerText, myNameOffset.left, myNameOffset.bottom + pageYOffset)
+
+    ctx.font = taglineFontSize + " " + taglineFontFamily;
+    ctx.fillStyle = "White";
+    ctx.textBaseline = "bottom";
+    ctx.fillText(taglineText.innerText, taglineOffset.left, taglineOffset.bottom + pageYOffset)
+
+  },
+  this.draw();
+}
+
+var canvasBackground = new CanvasBackground();
 ctx.globalCompositeOperation = "source-atop";
-
-var myNameText = document.getElementById("myName");
-var myNameOffset = getOffset(myName);
-var myNameFontSize = window.getComputedStyle(myNameText).fontSize;
-var myNameFontFamily = window.getComputedStyle(myNameText).fontFamily;
-
-var taglineText = document.getElementById("tagline");
-var taglineOffset = getOffset(tagline);
-var taglineFontSize = window.getComputedStyle(taglineText).fontSize;
-var taglineFontFamily = window.getComputedStyle(taglineText).fontFamily;
-
-ctx.beginPath();
-ctx.font = myNameFontSize + " " + myNameFontFamily;
-ctx.fillStyle = "White";
-ctx.textBaseline = "bottom";
-ctx.fillText(myNameText.innerText, myNameOffset.left, myNameOffset.bottom + pageYOffset)
-
-ctx.font = taglineFontSize + " " + taglineFontFamily;
-ctx.fillStyle = "White";
-ctx.textBaseline = "bottom";
-ctx.fillText(taglineText.innerText, taglineOffset.left, taglineOffset.bottom + pageYOffset)
-ctx.closePath();
+var canvasText = new CanvasText();
 
 // scroll event
 window.addEventListener("scroll", function () {
+  var headerTextSticky = false;
+
   if (headerTextOffset.bottom > headerOffset.bottom - pageYOffset) {
     headerText.setAttribute("style", "position: absolute; top: " + (headerOffset.bottom - (headerTextOffset.height/2)) +"px");
+    headerTextSticky = true;
   } else if (headerTextOffset.bottom < headerOffset.bottom - pageYOffset) {
     headerText.setAttribute("style", "position: fixed; top: 50%");
+    headerTextSticky = false;
   }
+
+  if (!headerTextSticky) {
+    ctx.clearRect(0, 0, canvas.innerWidth, canvas.innerHeight);
+    canvasBackground.draw();
+    canvasText.draw();
+  }
+
 });
