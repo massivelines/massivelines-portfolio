fcb4d4b0999940276f2fdf67b320bbb2948744e6
Shawn Rhodes <shawn@massivelines.com>
2020-04-26 17:24:22 -0500

    Update file folders

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,11 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [".storybook/**", "**/*.stories.js"],
+      },
+    ],
   },
-}
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,67 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Shawn Rhodes`,
+    description: `Shawn Rhodes - Front End Developer & Web Designer`,
+    author: `@massivelines`,
   },
   plugins: [
+    // 'gatsby-plugin-svgr',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+        // path: `${__dirname}/src/content/assets`,
       },
     },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     path: `${__dirname}/src/content/projects`,
+    //     name: `projects`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Shawn Rhodes`,
+        short_name: `Shawn Rhodes`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#b8312d`,
+        theme_color: `#b8312d`,
         display: `minimal-ui`,
+        // icon: `src/content/assets/code-regular.png`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // {
+    //   resolve: `gatsby-transformer-remark`,
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: `gatsby-remark-images`,
+    //         options: {
+    //           maxWidth: 768,
+    //         },
+    //       },
+    //       `gatsby-remark-copy-linked-files`,
+    //       `gatsby-remark-smartypants`,
+    //       'gatsby-remark-component',
+    //     ],
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `UA-114418107-1`,
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
